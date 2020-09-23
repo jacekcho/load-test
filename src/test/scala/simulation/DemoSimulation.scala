3843fab1987b7b05ffaa981d006115af448ffccb
Jacek Chowaniec <jacek.chowaniec@grandparade.co.uk>
2020-09-23 12:15:02 +0200

    add System properties

--- src/test/scala/simulation/DemoSimulation.scala
+++ src/test/scala/simulation/DemoSimulation.scala
@@ -7,20 +7,29 @@ import scala.concurrent.duration._
 
 class DemoSimulation extends Simulation {
 
-  val RAMPS = 2
-  val maxUsers = 8
-  val paceDurationMinutes = 2
-  val rampDurationMinutes = 1
+  val ramps = Integer.parseInt(System.getProperty("ramps", "2"))
+  val users = Integer.parseInt(System.getProperty("users", "8"))
+  val paceDurationMinutes = Integer.parseInt(System.getProperty("pace_duration", "2"))
+  val rampDurationMinutes = Integer.parseInt(System.getProperty("ramp_duration", "1"))
+
+  val usersPerRamp = users / ramps
+  val maxDuration = paceDurationMinutes * ramps + rampDurationMinutes * ramps
+
+  Console.println("Max users: " + users)
+  Console.println("Number of ramps: " + ramps)
+  Console.println("Users per ramps: " + usersPerRamp)
+  Console.println("Single ramp duration in minutes: " + rampDurationMinutes)
+  Console.println("Pace duration in minutes: " + paceDurationMinutes)
 
   setUp(
     firstTestScenario
       .inject(
-        rampUsers(maxUsers / RAMPS) during (rampDurationMinutes minutes),
+        rampUsers(usersPerRamp) during (rampDurationMinutes minutes),
         nothingFor(paceDurationMinutes minutes),
 
-        rampUsers(maxUsers / RAMPS) during (rampDurationMinutes minutes),
+        rampUsers(usersPerRamp) during (rampDurationMinutes minutes),
         nothingFor(paceDurationMinutes minutes)
       )
-  ).maxDuration((paceDurationMinutes * RAMPS + rampDurationMinutes * RAMPS) minutes)
+  ).maxDuration(maxDuration minutes)
 
 }
