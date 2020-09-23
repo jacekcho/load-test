56263c380c83a02a5a1c5aeeeddd5b98245d2411
Jacek Chowaniec <jacek.chowaniec@grandparade.co.uk>
2020-09-23 10:53:17 +0200

    refactoring

---
+++ src/test/scala/scenarios/FirstScenario.scala
@@ -0,0 +1,42 @@
+package scenarios
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object FirstScenario {
+
+  val headers = Map(
+    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
+  )
+
+  val protocol = "http://"
+  val homeUrl = "computer-database.gatling.io"
+
+  val firstTestScenario = scenario("Scenario name")
+    .forever {
+      exec(session => session.reset)
+        .exec(flushHttpCache)
+        .exec(flushCookieJar)
+        .exec(flushSessionCookies)
+        .repeat(1) {
+          exec(
+            http("first request description")
+              .get(protocol + homeUrl + "/computers")
+              .headers(headers)
+              .check(status.in(200, 304))
+          )
+            .pause(1)
+        }
+        .repeat(3) {
+          exec(
+            http("second request description")
+              .get(protocol + homeUrl + "/computers/381")
+              .headers(headers)
+              .check(status.in(200, 304))
+          )
+            .pause(1)
+        }
+    }
+}
